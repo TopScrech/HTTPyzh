285c6d35c2da26e89b5a843007bf8ee9dc821297
TopScrech <sergei_saliukov@icloud.com>
2025-08-12 14:19:59 +0300

    minor changes here and there

--- HTTPyzh/HTTPyzhApp.swift
+++ HTTPyzh/HTTPyzhApp.swift
@@ -5,7 +5,7 @@ import SwiftData
 struct HTTPyzhApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            //            Item.self,
+            //Item.self,
         ])
         
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
